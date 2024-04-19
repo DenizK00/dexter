864066188790d334d91d929dc7349229347dfb08
Deniz Kurtaran <denizkurtaran00@gmail.com>
2024-04-19 23:38:06 +0200

    Update parsing

--- OPT/Equation.py
+++ OPT/Equation.py
@@ -8,43 +8,55 @@ Created on Thu Apr 11 19:46:22 2024
 
 import re
 import numpy as np
+import pandas as pd
 
 class Equation:
-    def __init__(self, expr: str):
+    def __init__(self, expr: str, name=None):
         self.expr = expr
         self.terms = self.parse_terms()
-        self.variables, self.coefficients = self.extract_terms()
+        self.var_to_coef = self.extract_terms()
 
-    def parse_terms(self):
-        exprs = self.expr.split("=")
+
+    def __repr__(self) -> str:
+        return self.expr
+    
+
+    def parse_terms(self) -> list[tuple]:
+
+        seperator_pattern = r"(>=|<=|=)"
+        exprs = re.split(seperator_pattern, self.expr)
+
         self.LHS = exprs[0]
-        self.RHS = exprs[1] if len(exprs) == 2 else None
+        [self.SEP, self.RHS] = [exprs[1], exprs[2]] if len(exprs) == 3 else [None, None]
 
         # Regex to capture terms with optional leading signs, coefficients, and variable names
-        pattern = r'([+-]?)\s*(\d+)([a-zA-Z]+)'
+        pattern = r"([+-]?)\s*(\d+)([a-zA-Z]+)"
         return re.findall(pattern, self.LHS)
-
-
-    def extract_terms(self):
-        var_to_coef = {t[2]:float(t[1])*(-1 if t[0] == "-" else 1) for t in self.terms}
-
-        variables = np.array(list(var_to_coef.keys()))
-        coefficients = np.array(list(var_to_coef.values()))
-
-        return variables, coefficients
-
-
-    def to_Vector(self):
-        # Create a list of coefficients based on sorted variables
-        row = [self.coefficients.get(var, 0) for var in sorted(self.variables)]
-        row.append(self.coefficients.get('constant', 0))  # Append the constant term
-        return np.array(row) 
     
-    def to_Series(self):
-        pass
 
+    def extract_terms(self) -> dict[str: float]:
+        var_to_coef = {t[2]:float(t[1])*(-1 if t[0] == "-" else 1) for t in self.terms}
+        
+        match self.SEP:
+            case ">=":
+                var_to_coef["s"] = 1
+            case "=":
+                pass
+
+        self.variables = list(var_to_coef.keys())
+        self.coefficients = np.array(list(var_to_coef.values()))
+
+        return var_to_coef
+    
+
+    def to_vec(self, sort=True) -> np.array:    
+        return np.array(self.coefficients) 
+    
+
+    def to_row(self, sort=True) -> pd.DataFrame:
+        return pd.DataFrame([self.coefficients], columns=self.variables)
 
 # Example usage
-equation = "3x_1 + 5y - 2z - 4a"
+equation = "3x + 5y - 2z >= 10"
 eq = Equation(equation)
-print(eq.variables)
+print(eq.to_row())
--- OPT/Problem.py
+++ OPT/Problem.py
@@ -1,3 +1,11 @@
+from Equation import Equation
+
 class Problem:
     def __init__(self, objective:str, constraints:list[str]):
-        self.objective = Equation(objective.strip("max", "min"))
+        self.objective = Equation(objective.upper().strip("max"))
+        self.constraints = [Equation(c) for c in constraints]
+
+
+
+p1 = Problem(objective="Max 3x + 5y", constraints=["2x + 10y <= 20"])
+print(p1.objective)
