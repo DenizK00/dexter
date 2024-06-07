8cbe65e2a6840c2ecd141a7f71ddb3ff01db9196
Deniz Kurtaran <denizkurtaran00@gmail.com>
2024-06-07 20:38:49 +0200

    Reset Repo
    Focusing on grid representation first

--- Env/Grid/grid/main.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from grid.draw_grid import DrawGrid
-
-def draw(width=10, height=10, cellsize=30):
-    root = tk.Tk()
-    root.title("Grid Environment Drawer")
-    app = DrawGrid(root, width=width, height=height, cell_size=cellsize)
-    root.mainloop()
