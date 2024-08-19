f1fdeaef6a42d0bc0b6bf0fb325ec197100c5dcc
Deniz Kurtaran <denizkurtaran00@gmail.com>
2024-08-19 14:49:26 +0200

    Add test for Equation
    - simple test for parsing-check

--- OPT/tests/test_eq.py
+++ OPT/tests/test_eq.py
@@ -10,8 +10,19 @@ import unittest
 from equation import Equation
 
 class TestRV(unittest.TestCase):
-    def test_expected_value_1(self):
-        Equation("2x + 5y = 5")
+    def test_parsing_1(self):
+        eq = Equation("2*x + 5*y >= 5", name="example")
+
+        """Check initialization"""
+        assert(isinstance(eq, Equation))
+        assert(eq.name == "example")
+
+        """Check variable and coefficient read"""
+        assert(eq.var_to_coef == {"x":2, "y":5})
+
+    
+
+
 
 if __name__ == '__main__':
     unittest.main()
